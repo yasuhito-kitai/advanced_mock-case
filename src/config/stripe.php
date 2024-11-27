f66fab6ea3ac5b58eaa6ce2e9ef9cc163fbda9ec
yasuhito kitai <y.kitai.1124@gmail.com>
2024-11-27 13:19:30 +0900

    vendor内fortifyフォルダを追跡対象に変更

--- src/config/stripe.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'public_key' => env('STRIPE_KEY'),
-    'secret_key' => env('STRIPE_SECRET'),
-    'price_id' => env('STRIPE_PRICE_ID'),
-];
--- src/routes/console.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Schedule;
-
-
-Schedule::command('command:sendEmails')->dailyAt('7:00');
